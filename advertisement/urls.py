4c9f19b914247e79478539356b02caf6f98aa0ef
Ali <heydari1375.ali@gmail.com>
2025-01-28 11:45:12 +0330

    create view and url and template for showing ads in categories

--- advertisement/urls.py
+++ advertisement/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('' , views.IndexView.as_view() , name='index'),
     path('ad-detail/<int:pk>',views.AdDetailView.as_view(),name='ad-detail'),
     path('ad-create/' , views.AdCreateFormView.as_view(),name='ad-create'),
+    path('ad-category/<str:name>/' , views.AdCategoryView.as_view(),name='ad-category')
 ]
--- advertisement/views.py
+++ advertisement/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render,get_object_or_404,redirect
 from django.urls import reverse
-from .models import Advertisement
+from .models import Advertisement ,Category
 from django.views import View
-from .forms import AdvertisementForm
+from .forms import AdvertisementForm 
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
@@ -14,7 +14,8 @@ class IndexView(View):
             ad_list = Advertisement.objects.filter(title__contains=search)
         else:
             ad_list = Advertisement.objects.all()
-        context = {'ad_list':ad_list}
+        categories = Category.objects.all()
+        context = {'ad_list':ad_list , 'categories' : categories}
         return render(request,'advertisement/index.html',context)
     
     
@@ -44,3 +45,15 @@ class AdCreateFormView(LoginRequiredMixin,View):
             context = {'form' : form}
             return render(request,'advertisement/ad_create.html',context)
         
+
+class AdCategoryView(View):
+    def get(self,request,name):
+        ads = Advertisement.objects.filter(category__name__contains = name)
+        categories = Category.objects.all()
+        category_name = Category.objects.filter(name__contains = name).first()
+        context = {
+            'ads' : ads ,
+            'categories' : categories,
+            'category_name' : category_name
+            }
+        return render(request,'advertisement/filter_result.html', context)
